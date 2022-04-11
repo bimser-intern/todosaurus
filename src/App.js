855f69c73a7dcef7dd8d71ee21168717666c3796
uyetiskin <ufukyetiskin@hotmail.com>
2022-04-11 16:28:37 +0300

    [feature]-[12]-[semver:major]-Categories componenti oluşturuldu. Tailwind ile biçimlendirme yapıldı.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
+import Categories from './components/Categories';
 import Headbar from './components/Headbar';
 import List from './components/List';
+
 
 
 
@@ -14,7 +16,7 @@ function App() {
         <Headbar ></Headbar>
       </div>
       <div className='flex justify-between'>
-        <div className='w-1/5' >Kategori</div>
+        <div className='w-1/5' ><Categories className='h-full'></Categories></div>
         <div className='w-4/5'><List  /></div>
       </div>
 
